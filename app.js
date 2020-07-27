11631f9659941bf0a3cf9030ed73aba4f428c843
rehman159 <rehman_uetpesh@yahoo.com>
2020-07-27 20:54:27 +0500

    loggings to get to know how system is working

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const command = process.argv[2];
 console.log(process.argv);
+
+if (command === "plus") {
+  console.log("Addition is going on ...");
+} else if (command === "minus") {
+  console.log("Substraction is going on ...");
+}

651b2770db10436df63808045da22f2ccda7ed2a
rehman159 <rehman_uetpesh@yahoo.com>
2020-07-29 22:00:48 +0500

    implement of commandline arguments with yargs

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const command = process.argv[2];
-console.log(process.argv);
+const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
 
-if (command === "plus") {
-  console.log("Addition is going on ...");
-} else if (command === "minus") {
-  console.log("Substraction is going on ...");
-}
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function () {
+    console.log("Listing out all notes");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+
+console.log(yargs.argv);
